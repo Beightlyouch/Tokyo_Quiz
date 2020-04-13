a8144b3bfbef79788bc79cdbaad1d544cd6ee686
Beightlyouch <beightlyouch@gmail.com>
2020-04-13 11:44:52 +0900

    add 雑ni

--- app/src/main/java/com/beightlyouch/tokyo/StartActivity.java
+++ app/src/main/java/com/beightlyouch/tokyo/StartActivity.java
@@ -7,8 +7,6 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.beightlyouch.yamaguchi.R;
-
 
 public class StartActivity extends AppCompatActivity {
 
